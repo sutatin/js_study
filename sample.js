1db7e6625f3370fb9aab36c8ca19cbb0610bc393
ishizawa_t <ishizawa_t@worksap.co.jp>
2014-03-06 19:55:56 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,15 @@
+//init
+ console.log("start");
+
+//bind
+ $("#button_1").click(function(){
+	console.log("hoge")});
+
+ $("#button_2").click(bindButton2);
+
+//end
+	console.log("end");
+
+//functions
+function bindButton2(){
+	console.log("hoge2")}
